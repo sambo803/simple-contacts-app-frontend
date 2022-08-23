a67dcc3317672aab9bc77c681d51052ca8c186a1
samuel <sbacon803@gmail.com>
2022-08-23 14:06:26 -0600

    add get index contacts action

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
+<script>
+import axios from 'axios';
+
+  export default {
+    data: function () {
+      return {
+        message: "Welcome to Vue.js!",
+        contacts: []
+      };
+    },
+    created: function () {},
+    methods: {
+      indexContacts: function() {
+        console.log('getting the data');
+        axios.get("http://localhost:3000/contacts.json").then(response => { console.log(response.data);
+        this.contacts = response.data;
+        })
+      }
+
+    },
+
+  };
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div v-for="contact in contacts">
+   <p>{{ contact }}</p>
+   <br />
+   </div>
+   <button v-on:click="indexContacts()">Get Contacts</button>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style></style>
